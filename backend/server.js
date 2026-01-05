8b94f7ab2d353baf8b2250693ebe991566cffccb
Nimira43 <n.rabone72@aol.com>
2026-01-05 07:27:37 +0000

    Setting up server, routes, controller, middleware and error handling in backend.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({exptended: false}))
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Querella Support Desk' })
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
