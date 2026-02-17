0f27a32d67895ddd9a0e336335a82d1f18a0f75c
Nimira43 <n.rabone72@aol.com>
2026-02-17 22:21:46 +0000

    Home, Login and Register pages.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import { Link } from 'react-router-dom'
+import { BsTicketPerforated, BsQuestionCircle} from "react-icons/bs"
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>Querella Helpdesk</h1>
+        <p>Please select an option from below</p>
+      </section>
+      <Link
+        to='new-ticket'
+        className='btn btn-reverse btn-block'
+      >
+        <BsQuestionCircle /> Create New Ticket
+      </Link>
+      <Link
+        to='tickets'
+        className='btn btn-block'
+      >
+        <BsTicketPerforated /> View Tickets
+      </Link>
+    </>
   )
 }
 
