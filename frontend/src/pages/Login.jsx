639296dcf105451825d656c5072b2cdbcb9bb016
Nimira43 <n.rabone72@aol.com>
2026-02-27 22:39:20 +0000

    Setting up Redux authSlice and hooking up Login and Register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../feature/auth/authSlice'
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -7,6 +9,8 @@ function Login() {
   })
 
   const {email, password} = formData
+  const dispatch = useDispatch()
+  const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -17,6 +21,12 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
   }
 
   return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../feature/auth/authSlice'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -10,6 +12,8 @@ function Register() {
   })
 
   const {name, email, password, password2} = formData
+  const dispatch = useDispatch()
+  const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -23,6 +27,13 @@ function Register() {
 
     if (password !== password2) {
       toast.error('Passwords do not match.')
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+      dispatch(register(userData))
     }
   }
 
