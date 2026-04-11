c5729239e002e3ce2abf4d8167eb72368e87e531
Nimira43 <n.rabone72@aol.com>
2026-04-11 04:34:34 +0100

    Create and get tickets

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -18,7 +18,7 @@ const ticketSchema = mongoose.Schema({
   status: {
     type: String,
     required: true,
-    enum: ['New', 'Open', 'Closed'],
+    enum: ['new', 'open', 'closed'],
     default: false, 
   },
 },
