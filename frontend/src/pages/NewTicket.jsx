aafa6585eaef0afa191fd193d757d1f855fef09a
Nimira43 <n.rabone72@aol.com>
2026-05-02 19:13:11 +0100

    Created NewTicket form.

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,8 +1,91 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
 function NewTicket() {
+  const { user } = useSelector((state) => state.auth)
+  const [ name ] = useState(user.name)
+  const [ email ] = useState(user.email)
+  const [ product, setProduct ] = useState('laptop')
+  const [ description, setDescription ] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      New Ticket
-    </div>
+    <>
+      <section className='heading'>
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+      <section className='form'>
+        <div className='form-group'>
+          <label htmlFor='name'>
+            Customer Name
+          </label>
+          <input
+            type='text'
+            value={name}
+            disabled
+            className='form-control'          
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>
+            Customer Name
+          </label>
+          <input
+            type='text'
+            value={email}
+            disabled
+            className='form-control'          
+          />
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='product'>
+              Product
+            </label>
+            <select
+              name='product'
+              id='product'
+              value={product}
+              onChange={(e) => setProduct(e.target.value)}
+            >
+              <option value='Laptop'>Laptop</option>
+              <option value='Desktop'>Desktop</option>
+              <option value='Smartphone'>Smartphone</option>
+              <option value='Printer'>Printer</option>
+              <option value='Accessories'>Accessories</option>
+              <option value='Software'>Software</option>
+              <option value='Login'>Login</option>
+              <option value='Network'>Network</option>
+              <option value='Other'>Other</option>
+            </select>
+          </div>
+          <div className='form-group'>
+            <label
+              htmlFor='description'
+            >
+              Description of Issue
+            </label>
+            <textarea
+              name='description'
+              id='description'
+              className='form-control'
+              placeholder='Description'
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
