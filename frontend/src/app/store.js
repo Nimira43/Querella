0841210f28182715ce90bfd23d0a817165a38c65
Nimira43 <n.rabone72@aol.com>
2026-05-12 08:37:55 +0100

    Fetching tickets from backend. Viewing tickets in the UI.

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -5,6 +5,6 @@ import ticketReducer from '../feature/tickets/ticketSlice'
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    ticket: ticketReducer
+    tickets: ticketReducer
   }
 })
